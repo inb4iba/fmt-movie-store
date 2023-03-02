ecbb17ea18786dfd56c86e71c8f6760bdb363cb0
Icaro <iba1987@gmail.com>
2023-03-02 00:59:04 -0300

    add style and update cards

---
+++ README.md
@@ -0,0 +1,1 @@
+reference: https://dribbble.com/shots/20292416-The-Trailers-Concept-Site-Part-1
--- index.js
+++ index.js
@@ -1,14 +1,58 @@
+import {
+  closedEyeSVG,
+  coloredHeartSVG,
+  coloredStarSVG,
+  heartSVG,
+  openEyeSVG,
+  starSVG,
+} from "./utils.js";
+
+class Movie {
+  title;
+  duration;
+  score;
+  #favorite;
+  #watched;
+
+  constructor(title, score, duration) {
+    this.title = title;
+    this.score = score;
+    this.duration = duration;
+    this.#favorite = false;
+    this.#watched = false;
+  }
+
+  get favorite() {
+    return this.#favorite;
+  }
+
+  set favorite(value) {
+    this.#favorite = value;
+  }
+
+  get watched() {
+    return this.#watched;
+  }
+
+  set watched(value) {
+    this.#watched = value;
+  }
+}
+
 const movies = [];
 
 document
   .querySelector("#movie-register form")
-  .addEventListener("submit", (e) => e.preventDefault());
+  .addEventListener("submit", (event) => {
+    console.log(event);
+    event.preventDefault();
+  });
 
 document.getElementById("search-input").addEventListener("input", (e) => {
   filterMovies(e.target.value);
 });
 
-function registerMovie() {
+window.registerMovie = () => {
   const movie = getMovieFromForm();
   if (isMovieRegistered(movie.title))
     return alert("Já possui um filme com esse mesmo título.");
@@ -18,6 +62,22 @@ function registerMovie() {
 
   clearForm();
   listMovies(movies);
+};
+
+window.toggleWatched = (title) => {
+  const index = getIndexOfMovie(title);
+  movies[index].watched = !movies[index].watched;
+  listMovies(movies);
+};
+
+window.toggleFavorite = (title) => {
+  const index = getIndexOfMovie(title);
+  movies[index].favorite = !movies[index].favorite;
+  listMovies(movies);
+};
+
+function getIndexOfMovie(title) {
+  return movies.findIndex((movie) => movie.title === title);
 }
 
 function getMovieFromForm() {
@@ -25,7 +85,7 @@ function getMovieFromForm() {
   const score = document.getElementById("movie-score-input").value;
   const duration = document.getElementById("movie-duration-input").value;
 
-  return { title, score, duration, favorite: false, watched: false };
+  return new Movie(title, score, duration);
 }
 
 function clearForm() {
@@ -42,19 +102,50 @@ function listMovies(movies) {
   document.getElementById("movies-container").innerHTML = movies
     .map(
       (movie) => `
-        <div class="card">
-            <div class="image-wrapper">
-                <img src="https://via.placeholder.com/200x120">
-            </div>
-            <div class="movie-details">
-                <p>${movie.title}</p>
-                <p class="score">Nota: <span>${movie.score}</span></p>
-                <p class="duration">Duração: <span>${movie.duration}</span></p>
-            </div>
+      <div class="card" id="${movie.title.toLowerCase()}">
+      <div class="image-wrapper">
+        <img
+          src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/everything-everywhere-all-at-once_mpmkbaml_500x749.jpg?v=1649865955"
+        />
+        <div class="movie-icons">
+          <button id="watched-btn" class="icon-btn" onclick="toggleWatched('${
+            movie.title
+          }')">
+            ${movie.watched ? openEyeSVG : closedEyeSVG}
+          </button>
+          <button id="favorite-btn" class="icon-btn" onclick="toggleFavorite('${
+            movie.title
+          }')">
+            ${movie.favorite ? coloredHeartSVG : heartSVG}
+          </button>
         </div>
+      </div>
+      <div class="movie-info">
+        <h4 class="movie-title">${movie.title}</h4>
+        <div class="movie-details">
+          <span class="duration">Duração: ${movie.duration}</span>
+          <div class="score">
+            ${getScore(movie.score)}
+          </div>
+        </div>
+      </div>
+    </div>
     `
     )
     .join("");
+}
+
+function getScore(score) {
+  let scoreHTML = "";
+  for (let i = 0; i < 5; i++) {
+    if (i < score) {
+      scoreHTML += coloredStarSVG;
+      continue;
+    }
+    scoreHTML += starSVG;
+  }
+
+  return scoreHTML;
 }
 
 function filterMovies(filter) {
